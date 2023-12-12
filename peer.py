c6bb145672250f62fb90124a3a528bcac473f44d
Victoriia-Ripka <vikaripka@gmail.com>
2023-12-12 18:25:52 +0100

    added constants, added list of files, added comunication, added comments,

--- peer.py
+++ peer.py
@@ -1,21 +1,43 @@
 import socket
+import sys
+
+# Constants for the Peer
+HOST = "127.0.0.1"
+PORT = 5000
+FILE_PATH = "upload/a.txt"
 
 class Peer:
     def __init__(self, host, port):
+        # Initialize the Peer with its host, port, and create a socket for communication
         self.host = host
         self.port = port
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((self.host, self.port))
-
+        self.socket.connect((self.host, self.port))  # Connect to the Tracker
+ 
     def download_file(self, output_path):
+        # Download a file from the Tracker and save it to the specified output_path
         with open(output_path, 'wb') as file:
+            # Receive data from the Tracker
             data = self.socket.recv(1024)
             while data:
+                # Write the received data to the file
                 file.write(data)
+                # Continue receiving data
                 data = self.socket.recv(1024)
+        # Close the socket after downloading the file
         print("File downloaded successfully.")
         self.socket.close()
 
 if __name__ == "__main__":
-    peer = Peer("127.0.0.1", 5000)
+    # Default values
+    HOST = "127.0.0.1"
+    PORT = 5000
+    FILE_PATH = "upload/a.txt"
+
+    # Check for command-line arguments
+    # if len(sys.argv) == 4:
+    #     HOST, PORT, FILE_PATH = sys.argv[1:]
+
+    # Create a Peer instance and initiate the download process
+    peer = Peer(HOST, int(PORT))
     peer.download_file("download/downloaded_file.txt")
--- tracker.py
+++ tracker.py
@@ -1,41 +1,78 @@
 import socket
 import threading
 
+# Constants for the Tracker
+HOST = "127.0.0.1"
+PORT = 5000
+FILE_PATH = "upload/a.txt"
+
 class Tracker:
     def __init__(self, host, port, file_path):
+        # Initialize the Tracker with host, port, file_path, and an empty list of peers
         self.host = host
         self.port = port
-        self.file_path = file_path
-        self.peers = []
+        # Mapping of file names to their paths
+        self.files = {"a.txt": "upload/a.txt", "b.txt": "upload/b.txt", "c.txt": "upload/c.txt"} 
+        # List to store connected Peers
+        self.peers = []  
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind((self.host, self.port))
 
     def start(self):
+        # Start listening for incoming connections
         self.server.listen()
         print(f"Tracker listening on {self.host}:{self.port}")
 
         while True:
+            # Accept incoming connection from a Peer
             client_socket, addr = self.server.accept()
-            peer = Peer(client_socket, addr, self.file_path)
-            self.peers.append(peer)
+            peer = Peer(client_socket, addr, self)
+            self.peers.append(peer) # Add the Peer to the list of connected Peers
             threading.Thread(target=peer.handle_peer).start()
 
 class Peer:
-    def __init__(self, socket, addr, file_path):
+    # Initialize the Peer with its socket, address, and the tracker
+    def __init__(self, socket, addr, tracker):
         self.socket = socket
         self.addr = addr
-        self.file_path = file_path
+        self.tracker = tracker
 
     def handle_peer(self):
+        # Handle the communication with the connected Peer
         print(f"Connection from {self.addr}")
-        with open(self.file_path, 'rb') as file:
+
+        # Open the file and send its content to the connected Peer
+        self.send_file_list()
+        file_name = self.receive_file_name()
+        if file_name in self.tracker.files:
+            file_path = self.tracker.files[file_name]
+            self.send_file(file_path)
+        else:
+            print(f"File '{file_name}' not found.")
+
+        # Close the socket after sending the file
+        self.socket.close() 
+    
+    def send_file_list(self):
+        # Send the list of available files to the Peer
+        file_list = ",".join(self.tracker.files.keys())
+        self.socket.send(file_list.encode())
+
+    def receive_file_name(self):
+        # Receive the selected file name from the Peer
+        file_name = self.socket.recv(1024).decode()
+        return file_name
+
+    def send_file(self, file_path):
+        # Send the selected file to the Peer
+        with open(file_path, 'rb') as file:
             data = file.read(1024)
             while data:
                 self.socket.send(data)
                 data = file.read(1024)
-        print(f"File sent to {self.addr}")
-        self.socket.close()
+        print(f"File '{file_path}' sent to {self.addr}")
 
 if __name__ == "__main__":
-    tracker = Tracker("127.0.0.1", 5000, "upload/a.txt")
+    # Create a Tracker instance and start listening for connections
+    tracker = Tracker(HOST, PORT, FILE_PATH)
     tracker.start()
